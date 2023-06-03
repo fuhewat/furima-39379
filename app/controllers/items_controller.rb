33ca1ebd1453df7a1232f42a32be82868a4e5975
fuhewat <fuhechan@yahoo.co.jp>
2023-06-03 21:50:56 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+
   def index
     @items = Item.all.order(created_at: :desc).includes(:user)
-
   end
 
   def new
@@ -11,11 +11,15 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-  if @item.save
-    redirect_to root_path
-  else
-    render :new
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
