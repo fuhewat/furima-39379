95c38e35e17cda2555c5beb72e241a7fc1cedeaf
fuhewat <fuhechan@yahoo.co.jp>
2023-06-05 15:22:57 +0900

    rubocopを使用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,20 +24,21 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless current_user == @item.user
-      redirect_to root_path
-      return
-    end
+    return if current_user == @item.user
+
+    redirect_to root_path
+    nil
   end
 
   def update
     @item = Item.find(params[:id])
-      if@item.update(item_params)
-          redirect_to item_path
-        else
-          render :edit
-        end
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
     end
+  end
+
   private
 
   def item_params
